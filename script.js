2bf4bf812d52a880ca9903291170e58f1f9d5eb7
Lo.Erc <l.ercoli@outlook.it>
2020-11-16 22:18:26 +0100

    mod

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 var body = document.body,
-    html = document.documentElement;
+	    html = document.documentElement;
 
 var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+	                       html.clientHeight, html.scrollHeight, html.offsetHeight );
 
+var cancelled = false;
 
-$(document).ready(function(){
+var ripetizioni = 4
 
-	for (let i = 0; i < 1000; i++){
+function mouseDown() {
+	var tempScrollTop = $(window).scrollTop();
+	console.log("Scroll from Top: " + tempScrollTop.toString());
+	$('body,html').animate({scrollTop: (tempScrollTop)}, 0);
+	var cancelled = true;
+}
 
+function mouseUp(){
+	if (cancelled){
+		console.log("esci");
+    	return;
+  	} 
 
-		$('body,html').animate({scrollTop: (height)}, 3000);
-		$('body,html').animate({scrollTop: (-height)}, 0);		 
+  	var caso = Math.floor(Math.random() * height)
+	console.log(caso)
 
+  	for (x = 1; x < ripetizioni; x ++){
+
+  		if (x = ripetizioni-1){
+  			$('body,html').animate({scrollTop: (caso)}, 1000);
+  		}else{
+			$('body,html').animate({scrollTop: (height)}, 1000);
+			$('body,html').animate({scrollTop: (-height)}, 0);
+		}
 	}
 
-});
+}
 
 
+function longRunningFunction() {
+
+
+  console.log("entra")
+
+  // do some work, but not all
+  // save your progress to be able to resume when called again
+
+  if (!done) {
+    // release control, so that handlers can be called, and continue in 10ms
+    setTimeout(longRunningFunction, 10);
+  }
+}
